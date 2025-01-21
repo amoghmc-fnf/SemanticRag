76b66f0c1e23c9d947d3c2ba34588f05db4840de
Amogh M Cheluvaraj <Amogh.MCheluvaraj@fnf.com>
2025-01-21 15:08:51 +0530

    Add project files.

---
+++ SemanticRag/HotelModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.VectorData;
+
+public class Hotel
+{
+    [VectorStoreRecordKey]
+    public ulong HotelId { get; set; }
+
+    [VectorStoreRecordData(IsFilterable = true)]
+    public string HotelName { get; set; }
+
+    [VectorStoreRecordData(IsFullTextSearchable = true)]
+    public string Description { get; set; }
+
+    [VectorStoreRecordVector(Dimensions: 4, DistanceFunction.CosineDistance, IndexKind.Hnsw)]
+    public ReadOnlyMemory<float>? DescriptionEmbedding { get; set; }
+
+    [VectorStoreRecordData(IsFilterable = true)]
+    public string[] Tags { get; set; }
+}
---
+++ SemanticRag/Program.cs
@@ -0,0 +1,32 @@
+﻿using Azure;
+using Azure.Search.Documents.Indexes;
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel.Connectors.AzureAISearch;
+using Microsoft.SemanticKernel.Connectors.AzureOpenAI;
+using Microsoft.SemanticKernel.Embeddings;
+
+namespace SemanticRag
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .AddUserSecrets<Program>();
+
+            var configuration = builder.Build();
+
+            // Connect to text embedding model.
+            var textEmbeddingService = new AzureOpenAITextEmbeddingGenerationService(
+                configuration["AzureTextEmbedding:DeploymentName"],
+                configuration["AzureTextEmbedding:Endpoint"],
+                configuration["AzureTextEmbedding:Key"]);
+
+            // Connect to vector store
+            var vectorStore = new AzureAISearchVectorStore(
+                new SearchIndexClient(
+                    new Uri(configuration["AzureAiSearch:Url"]),
+                    new AzureKeyCredential(configuration["AzureAiSearch:Key"])));
+        }
+    }
+}
