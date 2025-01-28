e781b4d7ec780f8f88c80c85f5ed2bf73d2fc1c2
Amogh M Cheluvaraj <Amogh.MCheluvaraj@fnf.com>
2025-01-28 11:44:50 +0530

    add email plugin

---
+++ EmailPlugin/Models/Email.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailPlugin.Models
+{
+    public class Email
+    {
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public required string From { get; set; } = string.Empty;
+
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public required string To { get; set; } = string.Empty;
+
+        public required DateTime Date { get; set; }
+
+        public required string Subject { get; set; }
+
+        public required string Body { get; set; }
+    }
+}
